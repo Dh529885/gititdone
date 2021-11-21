46e276a926c8710f3f35cbd84555c34c2a54c70c
Dh529885 <89321290+Dh529885@users.noreply.github.com>
2021-11-20 19:07:10 -0500

    update state and adding and completing tasks

--- components/TodoList.js
+++ components/TodoList.js
@@ -5,7 +5,7 @@ import TodoModal from "./TodoModal";
 
 export default class TodoList extends React.Component {
   state = {
-    showListVisible: false,
+    showListVisible: false
   };
 
   toggleListModal() {
@@ -14,19 +14,23 @@ export default class TodoList extends React.Component {
 
   render() {
     const list = this.props.list;
-    const completedCount = list.todos.filter((todo) => todo.completed).length;
-    const finishedSubTitle = list.todos.length - completedCount;
+
+    const completedCount = list.todos.filter(todo => todo.completed).length;
+    const remainingCount = list.todos.length - completedCount;
 
     return (
       <View>
         <Modal
-          animationType="fade"
+          animationType="slide"
           visible={this.state.showListVisible}
           onRequestClose={() => this.toggleListModal()}
         >
-          <TodoModal list={list} closeModal={() => this.toggleListModal()} />
+          <TodoModal
+            list={list}
+            closeModal={() => this.toggleListModal()}
+            updateList={this.props.updateList}
+          />
         </Modal>
-
         <TouchableOpacity
           style={[styles.listContainer, { backgroundColor: list.color }]}
           onPress={() => this.toggleListModal()}
@@ -34,13 +38,16 @@ export default class TodoList extends React.Component {
           <Text style={styles.listTitle} numberOfLines={1}>
             {list.name}
           </Text>
-          <View style={{ alignItems: "center" }}>
-            <Text style={styles.count}>{completedCount}</Text>
-            <Text style={styles.subtitle}>Left</Text>
-          </View>
-          <View style={{ alignItems: "center" }}>
-            <Text style={styles.count}>{finishedSubTitle}</Text>
-            <Text style={styles.subtitle}>Finished</Text>
+
+          <View>
+            <View style={{ alignItems: "center" }}>
+              <Text style={styles.count}>{remainingCount}</Text>
+              <Text style={styles.subtitle}>Remaining</Text>
+            </View>
+            <View style={{ alignItems: "center" }}>
+              <Text style={styles.count}>{completedCount}</Text>
+              <Text style={styles.subtitle}>Completed</Text>
+            </View>
           </View>
         </TouchableOpacity>
       </View>
--- components/TodoModal.js
+++ components/TodoModal.js
@@ -8,22 +8,37 @@ import {
   FlatList,
   KeyboardAvoidingView,
   TextInput,
-  TouchableOpacityBase,
+  Keyboard,
 } from "react-native";
 import { colors, Icon } from "react-native-elements";
 import Colors from "../Colors";
 
 export default class TodoModal extends Component {
   state = {
-    name: this.props.list.name,
-    color: this.props.list.color,
-    todos: this.props.list.todos,
+    newTodo: "",
   };
 
-  renderTodo = (todo) => {
+  toggleTodoCompleted = (index) => {
+    let list = this.props.list;
+    list.todos[index].completed = !list.todos[index].completed;
+
+    this.props.updateList(list);
+  };
+
+  addTodo = () => {
+    let list = this.props.list;
+    list.todos.push({ title: this.state.newTodo, completed: false });
+
+    this.props.updateList(list);
+    this.setState({ newTodo: "" });
+
+    Keyboard.dismiss();
+  };
+
+  renderTodo = (todo, index) => {
     return (
       <View style={styles.todoContainer}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => this.toggleTodoCompleted(index)}>
           <Icon
             name={todo.completed ? "square" : "square-o"}
             type="font-awesome"
@@ -48,64 +63,65 @@ export default class TodoModal extends Component {
     );
   };
   render() {
-    const taskCount = this.state.todos.length;
-    const completedCount = this.state.todos.filter(
-      (todos) => todos.completed
-    ).length;
-    return (
-      <SafeAreaView style={styles.container}>
-        <TouchableOpacity
-          style={{ position: "absolute", top: 64, right: 32, zIndex: 10 }}
-          onPress={this.props.closeModal}
-        >
-          <Icon name="close" type="font-awesome" color={Colors.gold} />
-        </TouchableOpacity>
-        <View
-          style={[
-            styles.section,
-            styles.header,
-            { borderBottomColor: this.state.color },
-          ]}
-        >
-          <View>
-            <Text style={styles.title}>{this.state.name}</Text>
-            <Text style={styles.taskCount}>
-              {completedCount} of {taskCount} tasks
-            </Text>
-          </View>
-        </View>
+    const list = this.props.list;
 
-        <View style={[styles.section, { flex: 3 }]}>
-          <FlatList
-            data={this.state.todos}
-            renderItem={({ item }) => this.renderTodo(item)}
-            keyExtractor={(item) => item.title}
-            contentContainerStyle={{
-              paddingHorizontal: 32,
-              paddingVertical: 64,
-            }}
-            showsVerticalScrollIndicator={false}
-          />
-        </View>
-        <KeyboardAvoidingView
-          style={[styles.section, styles.footer]}
-          behavior="padding"
-        >
-          <TextInput
-            style={[styles.input, { borderColor: this.state.color }]}
-          />
+    const taskCount = list.todos.length;
+    const completedCount = list.todos.filter((todo) => todo.completed).length;
+
+    return (
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+        <SafeAreaView style={styles.container}>
           <TouchableOpacity
-            style={[styles.addTodo, { backgroundColor: this.state.color }]}
+            style={{ position: "absolute", top: 64, right: 32, zIndex: 10 }}
+            onPress={this.props.closeModal}
           >
-            <Icon
-              name="plus"
-              type="font-awesome"
-              color={colors.black}
-              onPress={() => this.toggleAddTodoModal()}
-            />
+            <Icon name="close" type="font-awesome" color={Colors.gold} />
           </TouchableOpacity>
-        </KeyboardAvoidingView>
-      </SafeAreaView>
+          <View
+            style={[
+              styles.section,
+              styles.header,
+              { borderBottomColor: list.color },
+            ]}
+          >
+            <View>
+              <Text style={styles.title}>{list.name}</Text>
+              <Text style={styles.taskCount}>
+                {completedCount} of {taskCount} tasks
+              </Text>
+            </View>
+          </View>
+
+          <View style={[styles.section, { flex: 3 }]}>
+            <FlatList
+              data={list.todos}
+              renderItem={({ item, index }) => this.renderTodo(item, index)}
+              keyExtractor={(item) => item.title}
+              contentContainerStyle={{
+                paddingHorizontal: 32,
+                paddingVertical: 64,
+              }}
+              showsVerticalScrollIndicator={false}
+            />
+          </View>
+          <View style={[styles.section, styles.footer]}>
+            <TextInput
+              style={[styles.input, { borderColor: list.color }]}
+              onChangeText={text => this.setState({ newTodo: text })}
+              value={this.state.newTodo}
+            />
+            <TouchableOpacity
+              style={[styles.addTodo, { backgroundColor: list.color }]} onPress={() => this.addTodo()}
+            >
+              <Icon
+                name="plus"
+                type="font-awesome"
+                color={colors.black}
+              />
+            </TouchableOpacity>
+          </View>
+        </SafeAreaView>
+      </KeyboardAvoidingView>
     );
   }
 }
